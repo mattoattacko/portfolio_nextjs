7ddfd1fcd8e6140ac3209c3ebfed73d6d2e68b74
unknown <matt.mcquain@gmail.com>
2021-05-12 22:26:50 -0700

    footer complete

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -6,7 +6,30 @@ import FooterLogo from 'assets/logo.svg';
 
 export default function Footer() {
   return (
-    <h1>Footer</h1>
+    <footer sx={styles.footer}>
+      <Container>
+        <Box sx={styles.footer.footerBottomArea}>
+          <Link>
+            <Image src={FooterLogo} alt="Our Logo" />
+          </Link>
+          <Box sx={styles.footer.menus}>
+            <nav>
+              {data.menuItem.map((item, i) => (
+                <Link 
+                  path={item.path}
+                  key={i}
+                  label={item.label}
+                  sx={styles.footer.link}
+                />
+              ))}
+            </nav>
+          </Box>
+          <Text sx={styles.footer.copyright}>
+            Copyright {new Date().getFullYear()} Petrolnaut
+          </Text>
+        </Box>
+      </Container>
+    </footer>
   );
 }
 
